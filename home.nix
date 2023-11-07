4bfbc98cc56cbecd55faa77c94a85880c1482057
Andy Zhang <1329212+zhangbanger@users.noreply.github.com>
2023-11-06 17:46:15 -0800

    Add intellij-idea-ultimate to packages

--- home.nix
+++ home.nix
@@ -17,7 +17,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -34,6 +34,7 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    jetbrains.idea-ultimate
   ];
   nixpkgs.config.allowUnfreePredicate = (pkg: true);
 
